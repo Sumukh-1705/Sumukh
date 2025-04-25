45e71ed24347d62ba776ca7d744a378a42b49f45
Sumukh-1705 <23aiml070@bnmit.in>
2025-04-25 09:45:42 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,11 @@
-print("HI BNMIT");
+
+def find_largest(num1, num2, num3):
+    largest = max(num1, num2, num3)
+    return largest
+
+num1 = 10
+num2 = 25
+num3 = 15
+
+largest_number = find_largest(num1, num2, num3)
+print(f"The largest number is: {largest_number}")
